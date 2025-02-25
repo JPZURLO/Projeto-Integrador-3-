bc54764c0c3f4d1cc7f5d850168ed7cc060509f6
Joao Paulo <jpzurlo.jz@gmail.com>
2025-02-25 15:56:54 -0300

    Opção Sair implementada na tela pos login e bloqueio a pagina pos login apos logout no sistema

--- FRONT/js/scriptOpçõesObrasELogout.js
+++ FRONT/js/scriptOpçõesObrasELogout.js
@@ -7,3 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
         submenu.style.display = submenu.style.display === "block" ? "none" : "block";
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const submenuToggle = document.querySelector(".has-submenu2 > a");
+    
+    submenuToggle.addEventListener("click", function (event) {
+        event.preventDefault();
+        const submenu = this.nextElementSibling;
+        submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+    });
+});
